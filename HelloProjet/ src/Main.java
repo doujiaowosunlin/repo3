801267db6e5942ecf707ff242cfbb2c4fe61903b
孙麟 <45112409+doujiaowosunlin@users.noreply.github.com>
2018-12-29 16:09:51 +0800

    Update Main.java

--- HelloProjet/ src/Main.java
+++ HelloProjet/ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        系统。出。println（“HelloWorld！”ououtyutututy）;
+        System.out.println(“HelloWorld!”);
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
