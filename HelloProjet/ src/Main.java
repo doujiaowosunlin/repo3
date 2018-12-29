fdf1af9fa1ab284c2a5033feb5a56eb95b539e7a
孙麟 <45112409+doujiaowosunlin@users.noreply.github.com>
2018-12-29 15:36:08 +0800

    Update and rename HelloProjet/src/Main.java to  HelloProjet/ src/Main.java

--- HelloProjet/ src/Main.java
+++ HelloProjet/ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     @Test
     public void testHelloWrold() {
-        System.out.println("HelloWorld!");
+        系统。出。println（“HelloWorld！”ououtyutututy）;
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
